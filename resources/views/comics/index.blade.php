8e9a9907a3dde96808f07d2d661306db698f8fe1
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-12 16:31:34 +0200

    add index.blade, print comics

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.main')
+
+@section('content')
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Series</th>
+        <th scope="col">Type</th>
+        <th scope="col">Sale Date</th>
+        <th scope="col">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($comics as $comic)
+        <tr>
+          <td>{{ $comic->id }}</td>
+          <td>{{ $comic->title }}</td>
+          <td>{{ $comic->series }}</td>
+          <td>{{ $comic->type }}</td>
+          <td>{{ $comic->sale_date }}</td>
+          <td>{{ $comic->price }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar">
   <ul class="nav nav-tabs w-100">
     <li class="nav-item">
-      <a class="nav-link active" href="#">Home</a>
+      <a class="nav-link active" href="{{ route('home') }}">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">List</a>
+      <a class="nav-link" href="{{{ route('comics.index') }}}">List</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Add</a>
