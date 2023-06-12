3827eb34483ea8f3cd29cca3e83d64fa64f85792
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-12 15:51:27 +0200

    comics migrate

--- database/migrations/2023_06_12_132320_create_comics_table.php
+++ database/migrations/2023_06_12_132320_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 20);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 5, 2);
+            $table->string('series', 20);
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
