daee7fc16c52db31e4d3c346e177ef74bf794980
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-12 15:41:10 +0200

    add view home, header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('content')
+  <div class="container text-center my-5">
+    <h1>HOME</h1>
+  </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar">
+  <ul class="nav nav-tabs w-100">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">List</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Add</a>
+    </li>
+  </ul>
+</nav>
+
