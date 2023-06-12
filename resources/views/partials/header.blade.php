579ba91419fe6648efb203b254975a5a833b0ae4
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-12 17:16:03 +0200

    add active navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,16 @@
 <nav class="navbar">
   <ul class="nav nav-tabs w-100">
     <li class="nav-item">
-      <a class="nav-link" href="{{ route('home') }}">Home</a>
+      <a class="nav-link {{ Route::currentRouteName() === 'home' ? 'active' : ''}}" href="{{ route('home') }}">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{{ route('comics.index') }}}">List</a>
+      <a class="nav-link {{ Route::currentRouteName() === 'comics.index' ? 'active' : ''}}" href="{{{ route('comics.index') }}}">List</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Add</a>
     </li>
   </ul>
+
+
 </nav>
 
